emberjs/data

Add hook for preserving dirty children on load
This is specifically needed for the REST adapter to ensure that dirty
children are preserved when new parent data comes back from the server.
Because the children are responsible for saving the relationship, the
parent record on the server won't yet know about the children. If the
children are dirty, it means they will be persisted to the server on
their transaction's next commit, at which point the server will then
know about them.

packages/ember-data/lib/adapters/rest_adapter.js
@@ -98,6 +98,10 @@ DS.RESTAdapter = DS.Adapter.extend({
     }, this);
   },
 
+  shouldPreserveDirtyRecords: function(relationship) {
+    return relationship.kind === 'hasMany';
+  },
+
   dirtyRecordsForRecordChange: function(dirtySet, record) {
     this._dirtyTree(dirtySet, record);
   },

mhelabs/ember-data

packages/ember-data/lib/adapters/rest_adapter.js
@@ -183,6 +183,10 @@ DS.RESTAdapter = DS.Adapter.extend({
     }
   },
 
+  shouldPreserveDirtyRecords: function(association) {
+    return association.kind === 'hasMany';
+  },
+
   /**
     Serializes the record and sends it to the server.
 